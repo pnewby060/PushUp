abb829a44e4e941b4ff3a7e7264aca4c984f234b
Philip Newby <pnewby060@gmail.com>
2017-04-23 02:23:38 +0100

    Added a yet another Method

--- app/src/main/java/uk/co/acleaninstall/pushup/MainActivity.java
+++ app/src/main/java/uk/co/acleaninstall/pushup/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private int YetAnother() {
         return 123;
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
